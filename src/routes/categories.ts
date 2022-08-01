da1e7c6e08c452762d13befb83b7b3748a64a474
alexh0810 <baohoan081097@gmail.com>
2022-08-01 19:54:19 +0300

    Added admin priviledge for post, put category

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import CategoryController from "../controllers/categoryController";
+import { verifyAdmin } from "../middlewares/userMiddleware";
 
 const categories = Router();
 categories.get("", CategoryController.getCategories);
 categories.get("/:id", CategoryController.getCategoryById);
-categories.post("", CategoryController.createCategory);
-categories.put("/:id", CategoryController.updateCategoryById);
+categories.post("", verifyAdmin, CategoryController.createCategory);
+categories.put("/:id", verifyAdmin, CategoryController.updateCategoryById);
 
 export default categories;
